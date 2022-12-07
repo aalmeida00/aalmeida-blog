1cec201d16b785dd094dad378d40b6c5ed0e5c15
Andre Almeida <andre-silva78@hotmail.com>
2022-12-06 23:09:19 -0300

    Fetch data from graphcms and render via getStaticProps on ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,47 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { GraphQLClient, gql } from 'graphql-request';
-import { useState } from 'react';
 
 const graphcms = new GraphQLClient(
   'https://api-sa-east-1.hygraph.com/v2/clbcwkx9h19zz01t914oy9q6f/master'
 );
 
-const QUERY = gql`  
-  posts {
-    content
-    createdAt
-    datePublished
-    id
-    publishedAt
-    slug
-    title
-    updatedAt
-    author {
-      name
-      avatar {
-        url
-      }
-    }
-    coverPhoto {
-      publishedAt {
-        createdBy {
-          id
+const QUERY = gql`
+  {
+    posts {
+      content
+      createdAt
+      datePublished
+      id
+      publishedAt
+      slug
+      title
+      updatedAt
+      author {
+        name
+        avatar {
+          url
         }
+      }
+      coverPhoto {
         url
       }
     }
   }
 `;
 
-export default function Home() {
-  const [posts, setPosts] = useState([]);
+export async function getStaticProps() {
+  const { posts } = await graphcms.request(QUERY);
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 10,
+  };
+}
+
+export default function Home(props: any) {
+  const { posts } = props;
 
   return (
     <div className={styles.container}>
@@ -45,7 +51,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        {posts.map((post: any) => (
+          <h1>{post.title}</h1>
+        ))}
+      </main>
     </div>
   );
 }
